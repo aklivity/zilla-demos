d2c64ef307a17288dac76ec4e017225ebcc59a35
AJ <vordimous@outlook.com>
2023-09-05 16:22:04 -0400

    add the grpc service proto and gateway

---
+++ taxi/grpc/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"net/http"
+
+	"github.com/golang/glog"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	gw "github.com/aklivity/zilla-demos/taxi/grpc/taxiroute" // Update
+)
+
+var (
+  // command-line options:
+  // gRPC server endpoint
+  grpcServerEndpoint = flag.String("grpc-server-endpoint",  "localhost:7114", "gRPC server endpoint")
+)
+
+func run() error {
+  ctx := context.Background()
+  ctx, cancel := context.WithCancel(ctx)
+  defer cancel()
+
+  // Register gRPC server endpoint
+  // Note: Make sure the gRPC server is running properly and accessible
+  mux := runtime.NewServeMux()
+  opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+  err := gw.RegisterTaxiRouteHandlerFromEndpoint(ctx, mux,  *grpcServerEndpoint, opts)
+  if err != nil {
+    return err
+  }
+
+  // Start HTTP server (and proxy calls to gRPC server endpoint)
+  glog.Info("running on :8081")
+  return http.ListenAndServe(":8081", mux)
+}
+
+func main() {
+  flag.Parse()
+  defer glog.Flush()
+
+  if err := run(); err != nil {
+    glog.Fatal(err)
+  }
+}
+
---
+++ taxi/startup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker-compose down --remove-orphans
+docker-compose build
+docker-compose up -d
