cb12aa211e99ee21f20d1294a8b2fd8d395dc9ec
AJ <vordimous@outlook.com>
2023-09-07 19:29:39 -0400

    bootstrap topic and write json

--- taxi/grpc/service/main.go
+++ taxi/grpc/service/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	// "bufio"
 	"context"
 	"flag"
+	"fmt"
 	"net"
 	"os"
+
+	// "os/exec"
 
 	pb "github.com/aklivity/zilla-demos/taxi/grpc/service/taxiroute"
 	"github.com/golang/glog"
@@ -23,13 +27,35 @@ type taxiRouteServer struct {
 
 func (s *taxiRouteServer) CreateTaxi(ctx context.Context, in *pb.Route) (*emptypb.Empty, error) {
 	defer glog.Flush()
-	err := os.WriteFile("test.json", []byte(protojson.Format(in)), 0644)
+	err := os.WriteFile(fmt.Sprintf("%.0f_route.json", in.GetTimestamp()), []byte(protojson.Format(in)), 0644)
 	return &emptypb.Empty{}, err
 }
 
 func main() {
 	flag.Parse()
 	defer glog.Flush()
+
+	// cmd := exec.Command("mqtt-simulator/main.py")
+	// stdout, err := cmd.StdoutPipe()
+	// if err != nil {
+	// 	glog.Fatal(err)
+	// }
+
+	// // start the command after having set up the pipe
+	// if err := cmd.Run(); err != nil {
+	// 	glog.Fatal(err)
+	// }
+
+	// // read command's stdout line by line
+	// in := bufio.NewScanner(stdout)
+
+	// for in.Scan() {
+	// 	glog.Info(in.Text()) // write each line to your log, or anything you need
+	// 	glog.Flush()
+	// }
+	// if err := in.Err(); err != nil {
+	// 	glog.Fatal(err)
+	// }
 
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
