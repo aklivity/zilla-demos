3596d00bb638b61693ab96223dc249ea4f07bea2
AJ <vordimous@outlook.com>
2023-09-08 09:10:54 -0400

    working sample

--- taxi/grpc/service/main.go
+++ taxi/grpc/service/main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	// "bufio"
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"net"
 	"os"
-
-	// "os/exec"
+	"os/exec"
 
 	pb "github.com/aklivity/zilla-demos/taxi/grpc/service/taxiroute"
 	"github.com/golang/glog"
@@ -21,41 +20,108 @@ const (
 	port = ":50051"
 )
 
+type dataConfig struct {
+	Name   string      `json:"NAME"`
+	Type   string      `json:"TYPE"`
+	Values [][]float64 `json:"VALUES"`
+}
+
+type topicConfig struct {
+	Type         string       `json:"TYPE"`
+	Prefix       string       `json:"PREFIX"`
+	TimeInterval int          `json:"TIME_INTERVAL"`
+	Data         []dataConfig `json:"DATA"`
+}
+
+type simulatorConfig struct {
+	BrokerURL       string        `json:"BROKER_URL"`
+	BrokerPort      int           `json:"BROKER_PORT"`
+	ProtocolVersion int           `json:"PROTOCOL_VERSION"`
+	CleanSession    bool          `json:"CLEAN_SESSION"`
+	Qos             int           `json:"QOS"`
+	Retained        bool          `json:"RETAINED"`
+	Topics          []topicConfig `json:"TOPICS"`
+}
+
 type taxiRouteServer struct {
 	pb.UnimplementedTaxiRouteServer
 }
 
 func (s *taxiRouteServer) CreateTaxi(ctx context.Context, in *pb.Route) (*emptypb.Empty, error) {
 	defer glog.Flush()
-	err := os.WriteFile(fmt.Sprintf("%.0f_route.json", in.GetTimestamp()), []byte(protojson.Format(in)), 0644)
-	return &emptypb.Empty{}, err
+	file, errs := os.CreateTemp("", fmt.Sprintf("%.0f-route-*.json", in.GetTimestamp()))
+	if errs != nil {
+		glog.Fatal(errs)
+	}
+	// defer os.Remove(file.Name())
+
+	inJson := protojson.Format(in)
+
+	var coords struct {
+		Values [][]float64 `json:"coordinates"`
+	}
+	json.Unmarshal([]byte(inJson), &coords)
+
+	simConfig := simulatorConfig{
+		BrokerURL:       "localhost",
+		BrokerPort:      1883,
+		ProtocolVersion: 5,
+		CleanSession:    false,
+		Qos:             0,
+		Retained:        true,
+		Topics: []topicConfig{
+			{
+				Type:         "single",
+				Prefix:       fmt.Sprintf("%.0f", in.GetTimestamp()),
+				TimeInterval: int(in.GetDuration() / float64(len(coords.Values))),
+				Data: []dataConfig{
+					{
+						Name:   "coordinate",
+						Type:   "raw_values",
+						Values: coords.Values,
+					},
+				},
+			},
+		},
+	}
+	jsonConfig, errs := json.Marshal(simConfig)
+	if errs != nil {
+		glog.Fatal(errs)
+	}
+	_, errs = file.Write(jsonConfig)
+	if errs != nil {
+		glog.Fatal(errs)
+	}
+	glog.Info(file.Name())
+	cmd := exec.Command("python3", "mqtt-simulator/main.py", "-f", file.Name())
+	// pipe, _ := cmd.StdoutPipe()
+	if err := cmd.Start(); err != nil {
+		glog.Fatal(err)
+		glog.Flush()
+	}
+	// reader := bufio.NewReader(pipe)
+	// line, err := reader.ReadString('\n')
+	// if err != nil {
+	// 	glog.Fatal(err)
+	// 	glog.Flush()
+	// }
+	// for err == nil {
+	// 	glog.Info(line)
+	// 	glog.Flush()
+	// 	line, err = reader.ReadString('\n')
+	// }
+
+	// save local
+	errs = os.WriteFile(fmt.Sprintf("%.0f_route.json", in.GetTimestamp()), jsonConfig, 0644)
+	if errs != nil {
+		glog.Fatal(errs)
+	}
+	return &emptypb.Empty{}, errs
 }
 
 func main() {
 	flag.Parse()
 	defer glog.Flush()
-
-	// cmd := exec.Command("mqtt-simulator/main.py")
-	// stdout, err := cmd.StdoutPipe()
-	// if err != nil {
-	// 	glog.Fatal(err)
-	// }
-
-	// // start the command after having set up the pipe
-	// if err := cmd.Run(); err != nil {
-	// 	glog.Fatal(err)
-	// }
-
-	// // read command's stdout line by line
-	// in := bufio.NewScanner(stdout)
-
-	// for in.Scan() {
-	// 	glog.Info(in.Text()) // write each line to your log, or anything you need
-	// 	glog.Flush()
-	// }
-	// if err := in.Err(); err != nil {
-	// 	glog.Fatal(err)
-	// }
 
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
