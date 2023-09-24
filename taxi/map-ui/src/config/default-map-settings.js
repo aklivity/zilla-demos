fa899bcd95710b6f0692d367a297a918df20881c
AJ <vordimous@outlook.com>
2023-09-24 14:46:12 -0400

    update routes

--- taxi/map-ui/src/config/default-map-settings.js
+++ taxi/map-ui/src/config/default-map-settings.js
@@ -33,7 +33,7 @@ const defaultMapSettings = {
   accessibleModeActive: false,
   shownOnceTooltips: {},
   // mapCenter: {lat: 37.32897976943132, lng: -121.88891172409059}, // Convention Center, San Jose, CA, USA
-  mapCenter: {lat: 37.333927711214876, lng: -121.88273191452028}, // downtown
+  mapCenter: {lat: 37.334678404943276, lng: -121.88837528228761}, // downtown
   defaultIsochroneColors: [  // qgis plugin color scheme as default
     '#2b83ba',
     '#64abb0',
