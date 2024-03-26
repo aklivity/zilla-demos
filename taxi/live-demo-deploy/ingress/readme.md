e5646227dec21cdfd09e96c97114a6494458374a
AJ <vordimous@outlook.com>
2024-03-26 16:41:46 -0400

    feat: update mentrics for remote write to grafana cloud

--- taxi/live-demo-deploy/ingress/readme.md
+++ taxi/live-demo-deploy/ingress/readme.md
@@ -1,9 +1,9 @@
 https://cert-manager.io/docs/tutorials/acme/nginx-ingress/#step-5---deploy-cert-manager
 
 
-kubectl get secrets taxi-demo-staging-tls -n taxi-demo -o json \
+kubectl get secrets taxi-demo-tls -n taxi-demo -o json \
  | jq 'del(.metadata["namespace","creationTimestamp","resourceVersion","selfLink","uid","annotations"])' \
  | kubectl apply -n monitoring -f -
 
 
-secret/taxi-demo-staging-tls created
+secret/taxi-demo-tls created
--- taxi/live-demo-deploy/ingress/setup.sh
+++ taxi/live-demo-deploy/ingress/setup.sh
@@ -23,9 +23,15 @@ kubectl create -f zilla-ingress-staging.yaml -n taxi-demo
 
 helm upgrade --install ingress-nginx ingress-nginx -n demos-ingress --create-namespace --repo https://kubernetes.github.io/ingress-nginx --wait \
     --set-string controller.service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-type"="nlb" 
-    # --set extraArgs.default-ssl-certificate="taxi-demo/taxi-demo-staging-tls" \
+    # --set extraArgs.default-ssl-certificate="taxi-demo/taxi-demo-tls" \
 #     --set tcp.7183="taxi-demo/zilla:7183"
 
 
 #     --set tcp.7114="taxi-demo/zilla:7114" \
 #     --set tcp.7151="taxi-demo/zilla:7151"
+
+
+ k delete ing web-app -n taxi-demo
+ k delete ing map-ui -n taxi-demo
+ k delete ing kafka-ui -n taxi-demo
+ k delete ing zilla -n taxi-demo
