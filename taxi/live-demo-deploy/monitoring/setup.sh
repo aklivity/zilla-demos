d58d7511544e7096375b88d9b7bb41d63f3dd7f2
AJ <vordimous@outlook.com>
2024-03-27 10:44:22 -0400

    feat: remote write prom and loki

--- taxi/live-demo-deploy/monitoring/setup.sh
+++ taxi/live-demo-deploy/monitoring/setup.sh
@@ -9,16 +9,20 @@ if [ -f .env ]; then
 fi
 
 NAMESPACE="${NAMESPACE:-monitoring}"
+PROM_USER="${PROM_USER:-1234}"
 PROM_PASS="${PROM_PASS:-admin}"
 GRAFANA_PASS="${GRAFANA_PASS:-admin}"
+LOKI_URL="${LOKI_URL:-localhost}"
 
 # kubectl create namespace $NAMESPACE
-# kubectl create -f pvc.yaml -n $NAMESPACE
+# kubectl create secret generic remotewrite-secret -n $NAMESPACE \
+#     --from-literal=username=$PROM_USER \
+#     --from-literal=password="$PROM_PASS"
 
 # Prometheus stack metrics collector
-helm upgrade --install prometheus-stack kube-prometheus-stack --version 57.0.3 -n $NAMESPACE --create-namespace --repo https://prometheus-community.github.io/helm-charts \
-    --values prometheus-stack-values.yaml \
-    --set grafana.adminPassword="$GRAFANA_PASS" \
-    --set spec.remoteWrite[0].basicAuth.password="$PROM_PASS"
+# helm upgrade --install prometheus-stack kube-prometheus-stack --version 57.0.3 -n $NAMESPACE --create-namespace --repo https://prometheus-community.github.io/helm-charts \
+#     --values prometheus-stack-values.yaml \
+#     --set-string grafana.adminPassword="$GRAFANA_PASS" \
 helm upgrade --install loki loki-stack -n $NAMESPACE --repo https://grafana.github.io/helm-charts \
-    --values loki-values.yaml
+    --values loki-values.yaml \
+    --set-string promtail.config.clients[0].url="$LOKI_URL"
