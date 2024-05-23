5c4ab2d64e2e9077c843f47c6282dd87336700b6
AJ <vordimous@outlook.com>
2024-05-23 10:02:38 -0400

    feat: get todomvc working with cqrs

--- todoMVC-CQRS/ui/src/components/TodosComponent.vue
+++ todoMVC-CQRS/ui/src/components/TodosComponent.vue
@@ -1,38 +1,14 @@
 <script setup>
-import { ref, computed, watch, onBeforeUpdate } from 'vue';
+import { ref, computed } from 'vue';
 import { useRoute } from 'vue-router';
-import {Buffer} from "buffer";
+import { Buffer } from "buffer";
 
 import TodoFooter from './TodoFooter.vue';
 import TodoHeader from './TodoHeader.vue';
 import TodoItem from './TodoItem.vue';
 
 const todos = ref([]);
-const todosRef = (el, index) => todos.value[index] = el
-onBeforeUpdate(() => todos.value = [])
-
-// This watcher will run on every update
-watch(
-  todos,
-  (currentTodos, previousTodos) => {
-    if (currentTodos.length !== previousTodos.length) {
-      // The number of elements has changed. Stale side effects
-      // need be cleaned up, and fresh side effects need to run.
-      return
-    }
-
-    // We've established that the number of elements is the same,
-    // but it's still possible that the elements could have changed
-    // order.
-    for (let i = 0; i < currentTodos.length; i++) {
-      if (currentTodos[i].isSameNode(previousTodos[i])) {
-        // The elements have changed order. Stale side effects
-        // need be cleaned up, and fresh side effects need to run.
-        return
-      }
-    }
-  }
-)
+const todosMap = {};
 const route = useRoute();
 const todoCommandUrl = "http://localhost:7114/tasks"
 
@@ -41,31 +17,25 @@ eventStream.addEventListener('delete', (event) => {
     let lastEventId = JSON.parse(event.lastEventId);
     let key = Buffer.from(lastEventId[0], "base64").toString("utf8");
     todos.value = todos.value.filter((t) => t.id !== key);
+    delete todosMap[key];
 }, false);
 
-// eventStream.onerror = function () {
-// };
+eventStream.onerror = console.log;
 
 eventStream.onmessage = function (event) {
     let lastEventId = JSON.parse(event.lastEventId);
     let key = Buffer.from(lastEventId[0], "base64").toString("utf8");
-    let todo = JSON.parse(event.data);
-    // let newTodo = !todosMap[key]
-    // todosMap[key] = {
-    //     ...todo,
-    //     "etag": lastEventId[1],
-    //     key,
-    // }
-    // if (newTodo) {
-    //     todos.value[key] = todosMap[key]
-    // }
-    todosRef({
-        ...todo,
+    let todo = {
+        ...JSON.parse(event.data),
         "etag": lastEventId[1],
-        key,
-    }, key)
+    };
 
-    console.log(todosMap[key]);
+    if (!todosMap[key]) {
+        todosMap[key] = todo;
+        todos.value.push(todo);
+    } else {
+        todos.value = todos.value.map((t) => t.id == key ? { ...t, ...todo } : t);
+    }
 };
 
 const filters = {
@@ -77,7 +47,7 @@ const filters = {
 const activeTodos = computed(() => filters.active(todos));
 const completedTodos = computed(() => filters.completed(todos));
 const filteredTodos = computed(() => {
-    switch(route.name) {
+    switch (route.name) {
         case "active":
             return activeTodos;
         case "completed":
@@ -113,11 +83,12 @@ function uuid() {
 
 function addTodo(value) {
     fetch(todoCommandUrl, {
-          method: "POST",
-          headers: {
+        method: "POST",
+        headers: {
             "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
+            "Idempotency-Key": `todoId-${uuid()}`,
+        },
+        body: JSON.stringify({
             title: value,
         })
     });
@@ -127,40 +98,38 @@ function deleteTodo(todo) {
     fetch(`${todoCommandUrl}/${todo.id}`, {
         method: "DELETE",
         headers: {
-          "Content-Type": "application/json",
-        //   "Idempotency-Key": uuid(),
+            "Content-Type": "application/json",
+            "Idempotency-Key": todo.id,
         }
     });
 }
 
 function toggleTodo(todo, value) {
-    // todo.completed = value;
     fetch(`${todoCommandUrl}/${todo.id}`, {
         method: "PUT",
         headers: {
-          "Content-Type": "application/json",
-        //   "Idempotency-Key": uuid(),
-        //   "If-Match": etag,
+            "Content-Type": "application/json",
+            "Idempotency-Key": todo.id,
+            "If-Match": todo.etag,
         },
-        body: JSON.stringify({...todo, completed: value})
+        body: JSON.stringify({ ...todo, completed: value })
     });
 };
 
 function editTodo(todo, value) {
-    // todo.title = value;
     fetch(`${todoCommandUrl}/${todo.id}`, {
         method: "PUT",
         headers: {
-          "Content-Type": "application/json",
-        //   "Idempotency-Key": uuid(),
-        //   "If-Match": etag,
+            "Content-Type": "application/json",
+            "Idempotency-Key": todo.id,
+            "If-Match": todo.etag,
         },
-        body: JSON.stringify({...todo, title: value})
+        body: JSON.stringify({ ...todo, title: value })
     });
 }
 
 function deleteCompleted() {
-    todos.value.filter(todo => !todo.completed).forEach(deleteTodo);
+    todos.value.filter(todo => todo.completed).forEach(deleteTodo);
 }
 </script>
 
@@ -168,7 +137,8 @@ function deleteCompleted() {
     <TodoHeader @add-todo="addTodo" />
     <main class="main" v-show="todos.length > 0">
         <div class="toggle-all-container">
-            <input type="checkbox" id="toggle-all-input" class="toggle-all" v-model="toggleAllModel" :disabled="filteredTodos.value.length === 0"/>
+            <input type="checkbox" id="toggle-all-input" class="toggle-all" v-model="toggleAllModel"
+                :disabled="filteredTodos.value.length === 0" />
             <label class="toggle-all-label" htmlFor="toggle-all-input"> Toggle All Input </label>
         </div>
         <ul class="todo-list">
