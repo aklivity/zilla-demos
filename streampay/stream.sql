9e87ac36829ad9355c11c80b8c9c8e87298681ba
Akram  Yakubov <akram.yakubov@gmail.com>
2024-07-31 12:46:32 -0700

    WIP

--- streampay/stream.sql
+++ streampay/stream.sql
@@ -1,83 +1,4 @@
--- CREATE TOPIC streampay-commands(
---   type VARCHAR,
-     userid VARCHAR,
-     requestid VARCHAR,
-     amount DOUBLE PRECISION,
-     notes VARCHAR
--- )
--- WITH (
---   kafka_topic='commands',
---   PARTITIONS=2,
---   VALUE_FORMAT=avro
--- );
---
---
--- CREATE TOPIC streampay-replies(
---  status VARCHAR,
-    correlationid VARCHAR
--- )
--- WITH (
---   PARTITIONS=2,
---   VALUE_FORMAT=avro
--- );
-
-
--- CREATE TOPIC streampay-payment-requests(
---  id VARCHAR,
---  fromUserId VARCHAR,
---  fromUserName VARCHAR,
---  toUserId VARCHAR,
---  toUserName VARCHAR,
---  amount DOUBLE PRECISION,
-    notes VARCHAR,
-    timestamp LONG
--- )
--- WITH (
---   PARTITIONS=2,
---   VALUE_FORMAT=avro
--- );
-
-
--- CREATE TOPIC streampay-users(
---  id VARCHAR,
-    name VARCHAR,
-    username VARCHAR
--- )
--- WITH (
---   PARTITIONS=2,
---   VALUE_FORMAT=avro
--- );
-
-
--- CREATE TOPIC streampay-balance-histories(
-    balance DOUBLE PRECISION,
-    timestamp LONG
--- )
--- WITH (
---   PARTITIONS=2,
---   VALUE_FORMAT=avro
--- );
-
-
-create function bad_request_status() returns varchar language javascript as $$
-    return '400';
-$$;
-
-create function success_request_status() returns varchar language javascript as $$
-    return '200';
-$$;
-
-create function generate_guid() returns varchar language javascript as $$
-    var result, i, j;
-    result = '';
-    for(j=0; j<32; j++) {
-        if( j == 8 || j == 12 || j == 16 || j == 20)
-          result = result + '-';
-        i = Math.floor(Math.random()*16).toString(16).toUpperCase();
-        result = result + i;
-    }
-  return result;
-$$;
+----------------------------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS users(
   *
@@ -96,7 +17,7 @@ WITH (
 CREATE TABLE IF NOT EXISTS commands
 INCLUDE KEY AS key
 INCLUDE header'zilla:correlation-id' AS correlation_id
-INCLUDE header 'identity' AS owenerid
+INCLUDE header 'identity' AS ownerid
 INCLUDE timestamp as timestamp
 WITH (
     connector='kafka',
@@ -105,10 +26,74 @@ WITH (
     scan.startup.mode='latest',
     scan.startup.timestamp.millis='140000000'
 ) FORMAT PLAIN ENCODE AVRO (
-    message = 'PaymentCommand',
     schema.registry = 'http://localhost:8081'
 );
 
+-----------------------------------------------------------
+
+CREATE TABLE streampay-commands(
+    type VARCHAR,
+    userid VARCHAR,
+    requestid VARCHAR,
+    amount DOUBLE PRECISION,
+    notes VARCHAR
+)
+INCLUDE zilla_correlation_id AS correlation_id
+INCLUDE zilla_identity AS ownerid
+INCLUDE timestamp as timestamp;
+
+CREATE TABLE streampay-replies(
+    status VARCHAR,
+    correlationid VARCHAR
+);
+
+CREATE TABLE streampay-payment-requests(
+  id VARCHAR,
+  fromUserId VARCHAR,
+  fromUserName VARCHAR,
+  toUserId VARCHAR,
+  toUserName VARCHAR,
+  amount DOUBLE PRECISION,
+  notes VARCHAR,
+  timestamp LONG
+);
+
+ CREATE TABLE streampay-users(
+  id VARCHAR primary_key,
+  name VARCHAR,
+  username VARCHAR
+ );
+
+CREATE TABLE streampay-balance(
+    user_id VARCHAR primary_key,
+    balance DOUBLE PRECISION,
+    timestamp LONG
+);
+
+CREATE TABLE streampay-balance-histories(
+    balance DOUBLE PRECISION,
+    timestamp LONG
+);
+
+CREATE FUNCTION bad_request_status() RETURNS VARCHAR LANGUAGE javascript AS $$
+    return '400';
+$$;
+
+CREATE FUNCTION success_request_status() RETURNS VARCHAR LANGUAGE javascript AS $$
+    return '200';
+$$;
+
+CREATE FUNCTION generate_guid() RETURNS VARCHAR LANGUAGE javascript AS $$
+    var result, i, j;
+    result = '';
+    for(j=0; j<32; j++) {
+        if( j == 8 || j == 12 || j == 16 || j == 20)
+          result = result + '-';
+        i = Math.floor(Math.random()*16).toString(16).toUpperCase();
+        result = result + i;
+    }
+  return result;
+$$;
 
 CREATE MATERIALIZED VIEW IF NOT EXISTS invalid_commands AS
     SELECT bad_request_status() as status, encode(correlation_id, 'escape') as correlationid from commands where key IS NULL OR type NOT IN ('SendPayment', 'RequestPayment');
@@ -117,6 +102,84 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS invalid_commands AS
 CREATE MATERIALIZED VIEW IF NOT EXISTS valid_commands AS
     SELECT success_request_status() as status,  encode(correlation_id, 'escape') as correlationid from commands where key IS NOT NULL AND type IN ('SendPayment', 'RequestPayment');
 
+CREATE MATERIALIZED VIEW withdrawals_transaction as
+SELECT
+    generate_guid() as id,
+    ownerid::varchar as ownerid,
+    -(cmd.amount) as amount,
+    cmd.timestamp as timestamp,
+    cmd.userid as userid
+FROM
+    (
+        SELECT
+            userid,
+            ownerid::varchar as ownerid,
+            amount,
+            timestamp
+        FROM
+            commands
+        WHERE
+        KEY IS NOT NULL
+        AND type = 'SendPayment'
+    ) as cmd
+    LEFT JOIN (
+        SELECT
+            userid,
+            balance
+        FROM
+            users_balance
+    ) AS ub ON cmd.ownerid = ub.userid AND ub.balance >= cmd.amount;
+
+
+CREATE MATERIALIZED VIEW deposit_transaction as
+SELECT
+    generate_guid() as id,
+    userid as fromUserId,
+    -(cmd.amount) as amount,
+    cmd.timestamp as timestamp,
+     ownerid::varchar as userid
+FROM
+    (
+        SELECT
+            userid,
+            ownerid::varchar as ownerid,
+            amount,
+            timestamp
+        FROM
+            commands
+        WHERE
+        KEY IS NOT NULL
+        AND type = 'SendPayment'
+    ) as cmd
+    LEFT JOIN (
+        SELECT
+            userid,
+            balance
+        FROM
+            users_balance
+    ) AS ub ON cmd.ownerid = ub.userid AND ub.balance >= cmd.amount;
+
+
+CREATE MATERIALIZED VIEW request_payment as
+SELECT
+    generate_guid() as id,
+    encode(ownerid, 'escape') as fromUserId,
+    u2.username as fromUsername,
+    userid as toUserId,
+    u1.username as toUsername,
+    amount,
+    notes
+FROM
+    commands
+JOIN
+    users u1 ON u1.id = commands.userid
+JOIN
+    users u2 ON u2.id = encode(commands.ownerid, 'escape')
+WHERE
+    key IS NOT NULL
+    AND type = 'RequestPayment';
+
+-----------------------------------------------------------------------------
 
 CREATE SINK invalid_replies
 FROM invalid_commands
@@ -142,84 +205,6 @@ ENCODE AVRO (
     schema.registry = 'http://localhost:8081'
 );
 
-CREATE MATERIALIZED VIEW withdrawals_transaction as
-SELECT
-    generate_guid() as id,
-    owenerid::varchar as owenerid,
-    -(cmd.amount) as amount,
-    cmd.timestamp as timestamp,
-    cmd.userid as userid
-FROM
-    (
-        SELECT
-            userid,
-            owenerid::varchar as owenerid,
-            amount,
-            timestamp
-        FROM
-            commands
-        WHERE
-        KEY IS NOT NULL
-        AND type = 'SendPayment'
-    ) as cmd
-    LEFT JOIN (
-        SELECT
-            userid,
-            balance
-        FROM
-            users_balance
-    ) AS ub ON cmd.owenerid = ub.userid AND ub.balance >= cmd.amount;
-
-
-CREATE MATERIALIZED VIEW deposit_transaction as
-SELECT
-    generate_guid() as id,
-    userid as fromUserId,
-    -(cmd.amount) as amount,
-    cmd.timestamp as timestamp,
-     owenerid::varchar as userid
-FROM
-    (
-        SELECT
-            userid,
-            owenerid::varchar as owenerid,
-            amount,
-            timestamp
-        FROM
-            commands
-        WHERE
-        KEY IS NOT NULL
-        AND type = 'SendPayment'
-    ) as cmd
-    LEFT JOIN (
-        SELECT
-            userid,
-            balance
-        FROM
-            users_balance
-    ) AS ub ON cmd.owenerid = ub.userid AND ub.balance >= cmd.amount;
-
-
-CREATE MATERIALIZED VIEW request_payment as
-SELECT
-    generate_guid() as id,
-    encode(owenerid, 'escape') as fromUserId,
-    u2.username as fromUsername,
-    userid as toUserId,
-    u1.username as toUsername,
-    amount,
-    notes
-FROM
-    commands
-JOIN
-    users u1 ON u1.id = commands.userid
-JOIN
-    users u2 ON u2.id = encode(commands.owenerid, 'escape')
-WHERE
-    key IS NOT NULL
-    AND type = 'RequestPayment';
-
-
 CREATE SINK request_payment_sink
 FROM request_payment
 WITH (
@@ -231,6 +216,8 @@ WITH (
 ENCODE AVRO (
     schema.registry = 'http://localhost:8081'
 );
+
+-----------------------------------------------------------------------------
 
 drop table users;
 drop table commands;
