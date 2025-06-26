4779c4a0ae5bb8b15bc9eaafaa556d07a390950f
Ankit Kumar <ankitjames25@gmail.com>
2025-06-26 21:40:53 +0530

    update Auth0 credentials

--- sports-betting/sports-betting-app/src/app/auth-provider-wrapper.tsx
+++ sports-betting/sports-betting-app/src/app/auth-provider-wrapper.tsx
@@ -4,8 +4,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 export default function Auth0ProviderWrapper({ children }) {
   return (
     <Auth0Provider
-      domain="dev-gw34352rmpxoiykv.us.auth0.com"
-      clientId="IJFowCCOx8pP5XzDIBJFr7wTcozdmbRe"
+      domain="aklivity.us.auth0.com"
+      clientId="FoHwttWzOuNqI1TBYWjDk6J0K3m9z3Vh"
       authorizationParams={{
         redirect_uri: typeof window !== 'undefined' ? window.location.origin : 'http://localhost:3000',
       }}
